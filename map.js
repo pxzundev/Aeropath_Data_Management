790187088cfdcf1c09f6736e8e575470375b2dd8
pxzundev <pxzundev@gmail.com>
2025-05-25 18:38:56 +1200

    update map.js
    Ruler display label fixed

--- map.js
+++ map.js
@@ -1,8 +1,8 @@
 // Initialize Leaflet map
-var map = L.map('map').setView([ -43.47498889, 172.54828611 ], 13);
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+var map = L.map("map").setView([-43.47498889, 172.54828611], 13);
+L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
   attribution: '&copy; <a href="https://carto.com/attributions">CARTO</a>',
-  maxZoom: 19
+  maxZoom: 19,
 }).addTo(map);
 
 // Assign map to window.map for global access
@@ -11,13 +11,14 @@ window.map = map;
 // Remove CDN ruler, use local static/ruler/leaflet-ruler.js
 // Add after map initialization
 var options = {
-    position: 'topleft',
-    lengthUnit: {
-        factor: 1000, // from km to m
-        display: 'm.',
-        decimal: 2,
-    },
+  position: "topleft",
+  lengthUnit: {
+    factor: 1000, // from km to m
+    display: "m.",
+    decimal: 2,
+    label: "Distance:",
+  },
 };
-if (typeof L.control.ruler === 'function') {
-    L.control.ruler(options).addTo(map);
+if (typeof L.control.ruler === "function") {
+  L.control.ruler(options).addTo(map);
 }
